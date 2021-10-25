0b0322feca1e20ae918bac1dff4056e184084e94
Wagner <wagnergoncalves84@gmail.com>
2021-10-25 10:13:04 -0300

    Fist commit

---
+++ src/br/com/dio/model/Main.java
@@ -0,0 +1,8 @@
+package br.com.dio.model;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/br/com/dio/model/PrimeiroPrograma.java
@@ -0,0 +1,20 @@
+package br.com.dio.model;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Dog dog = new Dog();
+
+        System.out.println(dog);
+
+
+
+
+       /* int a = 5;
+        int b = 3;
+
+        System.out.println("Hello World! " +  (a+b));*/
+
+
+    }
+}
